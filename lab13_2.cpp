2190d476ffe4088bf6fc8ea4131dce2236df66bd
solesicklab13 <solesick@cmu.ac.th>
2023-01-10 16:01:11 +0700

    solesicklab13_2_completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
-#include <ctime>
+
 using namespace std;
 
-long long fibonacci(long long int x);
+#include<iostream>
+using namespace std;
+
+int gcd(int,int);
 
 int main(){
-    clock_t start = clock();
-    cout << "Result: " << fibonacci(50) << "\n";
-    clock_t end = clock();
-    double elapsed = double(end - start)/CLOCKS_PER_SEC;
-    cout << "Elapsed Time: " << elapsed << " seconds.";
-    return 0;
+    int x, y;
+    cin >> x >> y;
+    cout << gcd(x,y);
+    return 0; 
 }
 
-long long fibonacci(long long int x){
-    if(x>1){
-        return fibonacci(x-1) + fibonacci(x-2);
-    }else if(x == 0){
-        return 0;
-    }else if(x == 1){
-        return 1;
+int gcd(int x,int y){
+    if(x%y == 0){
+        return y;
+    }else{
+        return gcd(y, x%y);
     }
-
-return 0;
 }
